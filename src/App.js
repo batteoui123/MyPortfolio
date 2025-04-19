94eb7c41fea54315b94b85d1caf7c1771055c21e
Lenovo <elbatteou.oussama@etu.uae.ac.ma>
2025-04-19 15:34:33 +0100

    add container-fluid

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="container">
+      <div className="container-fluid">
       <Banner  />
       <Skills />
       <Education/>
