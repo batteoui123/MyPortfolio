361b32a1ad473d8f8cd7016ecec02aa9ed6ef2de
Lenovo <elbatteou.oussama@etu.uae.ac.ma>
2025-04-19 18:01:03 +0100

    test contactnew2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -185,28 +185,49 @@ export const Contact = () => {
                 <h2>Get In Touch</h2>
                 <form onSubmit={handleSubmit}>
                   <Row>
+                 
+                
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                      <input type="text" value={formDetails.name} placeholder=" Name" onChange={(e) => onFormUpdate('name', e.target.value)}/>
                     </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
+      
                     <Col size={12} sm={6} className="px-1">
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
+                  
+                    <Col size={12} className=" bg-dange elmt">
+                        <textarea
+                          rows="6"
+                          value={formDetails.message}
+                          placeholder="Message"
+                          onChange={(e) => onFormUpdate("message", e.target.value)}
+                        ></textarea>
+                        <div className="d-flex bg-dange align-items-center mybtn ">
+                           <button type="submit text-center send " className="">
+                               <span>{buttonText}</span>
+                           </button>
+                       
+                             {status.message && (
+                    
+                                   <div className=" " style={{marginLeft:'auto'}}>
+                                 <p className={status.success === false ? "danger" : "success"}>
+                                    {status.message} 
+                                </p>
+                                </div>
+                      
+
+                            )} 
+                          
+                        </div>
+                        
+                      </Col>
+{/*                     
                     {
                       status.message &&
                       <Col>
                         <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
                       </Col>
-                    }
+                    } */}
                   </Row>
                 </form>
               </div>}
