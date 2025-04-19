49a92506bbc1aaace5cdc2f266661cb94ce7ac8d
Lenovo <elbatteou.oussama@etu.uae.ac.ma>
2025-04-19 16:14:56 +0100

    adjsting ......

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
   return (
     <div className="App">
       <NavBar />   
-      <Banner  />
+      {/* <Banner  />
       <Skills />
       <Education/>
       <Projects />
-      <Contact />
+      <Contact /> */}
     
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
